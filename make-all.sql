2d4c7ad2497b9e315065800676933f073cadf8e1
Alistair Johnson <alistairewj@gmail.com>
2016-08-11 16:25:37 -0400

    query to make subviews necessary for data.sql

---
+++ make-all.sql
@@ -0,0 +1,16 @@
+-- This function runs all the scripts necessary to generate the ALINE dataset.
+
+-- As the script is generating many materialized views, it may take some time.
+
+BEGIN;
+-- Generate the views
+
+\i sql/cohort.sql
+\i sql/echo-data.sql
+\i sql/icd.sql
+\i sql/labs.sql
+\i sql/sofa-aline
+\i sql/vaso-flg.sql
+\i sql/vitals.sql
+
+COMMIT;
